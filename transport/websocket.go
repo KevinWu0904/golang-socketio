8e860df9beb157407603e4c39d06d44d4aa02cc3
wuguohua.5281 <wuguohua.5281@bytedance.com>
2020-11-10 17:54:21 +0800

    feat: enable compression
    Change-Id: I8a3e7f8bd464739061ae81c5ec4da8ed59a1d63e

--- transport/websocket.go
+++ transport/websocket.go
@@ -93,7 +93,7 @@ type WebsocketTransport struct {
 }
 
 func (wst *WebsocketTransport) Connect(url string) (conn Connection, err error) {
-	dialer := websocket.Dialer{}
+	dialer := websocket.Dialer{EnableCompression:true}
 	socket, _, err := dialer.Dial(url, wst.RequestHeader)
 	if err != nil {
 		return nil, err
